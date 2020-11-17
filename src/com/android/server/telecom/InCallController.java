0af0c1d75d29c55128b2cbcd910a4de4f5249398
beanstown106 <nbenis106@gmail.com>
2020-11-17 12:49:53 +0100

    [2/3] Telecom: Add incall vibration options
     - allow setting vibration when call is connected
     - allow setting vibration when call is disconnected
     - allow setting vibration for call waiting
     - works with google and aosp dialer :)
    
    Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -34,8 +34,10 @@ import android.os.Looper;
 import android.os.RemoteException;
 import android.os.Trace;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telecom.CallAudioState;
 import android.telecom.ConnectionService;
+import android.telecom.DisconnectCause;
 import android.telecom.InCallService;
 import android.telecom.Log;
 import android.telecom.Logging.Runnable;
@@ -48,6 +50,7 @@ import com.android.internal.annotations.VisibleForTesting;
 // TODO: Needed for move to system service: import com.android.internal.R;
 import com.android.internal.telecom.IInCallService;
 import com.android.internal.util.IndentingPrintWriter;
+import com.android.internal.util.omni.OmniUtils;
 import com.android.server.telecom.SystemStateHelper.SystemStateListener;
 import com.android.server.telecom.ui.NotificationChannelManager;
 
@@ -70,6 +73,12 @@ import java.util.stream.Collectors;
 public class InCallController extends CallsManagerListenerBase {
     public static final int IN_CALL_SERVICE_NOTIFICATION_ID = 3;
     public static final String NOTIFICATION_TAG = InCallController.class.getSimpleName();
+
+    private static final long[] INCALL_VIBRATION_PATTERN = {
+            100,
+            200,
+            0,
+    };
 
     public class InCallServiceConnection {
         /**
@@ -1050,6 +1059,17 @@ public class InCallController extends CallsManagerListenerBase {
 
     @Override
     public void onCallStateChanged(Call call, int oldState, int newState) {
+        boolean vibrateOnConnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.OMNI_VIBRATE_ON_CONNECT, 0, UserHandle.USER_CURRENT) == 1;
+        boolean vibrateOnDisconnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.OMNI_VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrateOnConnect) {
+            OmniUtils.vibratePattern(mContext, INCALL_VIBRATION_PATTERN);
+        } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
+                && vibrateOnDisconnect) {
+            OmniUtils.vibratePattern(mContext, INCALL_VIBRATION_PATTERN);
+        }
         updateCall(call);
     }
 
